4ece2e4c3d5555c77f27a5cfb2e1919bdb62a1ca
saurabh <saurabhlively@yahoo.co.in>
2024-11-16 19:08:36 +0530

    added images

--- gui_todos.py
+++ gui_todos.py
@@ -8,7 +8,8 @@ clock = sg.Text('',key='clock')
 
 label = sg.Text("Type in a to-do")
 input_box=sg.InputText(tooltip="Enter do-do",key='todo')
-add_button = sg.Button("Add",size=10)
+add_button = sg.Button(size=2,image_source='add.png',mouseover_colors="LightBlue2",
+                       tooltip="Add Todo",key='Add')
 list_box = sg.Listbox(values=functions.get_todos(),key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
