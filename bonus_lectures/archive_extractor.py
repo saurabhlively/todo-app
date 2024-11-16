ac9758f889832fa8c8334724ad17fe45272abc36
saurabh <saurabhlively@yahoo.co.in>
2024-11-16 19:24:37 +0530

    Coded Archive extractor

---
+++ bonus_lectures/archive_extractor.py
@@ -0,0 +1,36 @@
+import FreeSimpleGUI as sg
+from zip_extractor import extract_archive
+
+sg.theme('DarkPurple4')
+
+label1 = sg.Text("Select Archive:")
+input1=sg.Input()
+choose_button1=sg.FileBrowse("Choose",key='archive')
+
+
+label2 = sg.Text("Select Path to decompress:")
+input2=sg.Input()
+choose_button2=sg.FolderBrowse("Choose",key='folder')
+
+extract_button=sg.Button("Extract")
+output_label = sg.Text(key="output",text_color='green')
+
+
+window = sg.Window("File Extractor",
+                   layout=[[label1,input1,choose_button1],
+                           [label2,input2,choose_button2],
+                           [extract_button,output_label]])
+
+while True:
+    event,values = window.read()
+    print(1,event)
+    print(2,values)
+    archivepath=values['archive']
+    dest_dir=values['folder']
+    extract_archive(archivepath,dest_dir)
+    window['output'].update(value="Extraction Completed")
+
+window.close()
+
+
+
