f5bca530a3620b9553eaad9fd1e3da2c0453634d
saurabh <saurabhlively@yahoo.co.in>
2024-11-16 19:38:29 +0530

    Added if filepath does not exist

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-def get_todos(filepath='files/todos.txt'):
+def get_todos(filepath='todos.txt'):
     """Read a text file and return the todos list"""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg,filepath='files/todos.txt'):
+def write_todos(todos_arg,filepath='todos.txt'):
     """Write a todos list to a file"""
     with open(filepath, 'w') as f:
         f.writelines(todos_arg)
