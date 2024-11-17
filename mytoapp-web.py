912de0417f233d4d5e7dce38e3cd9ce13967d340
saurabh <saurabhlively@yahoo.co.in>
2024-11-17 13:11:08 +0530

    Web interface execution with add button

--- mytoapp-web.py
+++ mytoapp-web.py
@@ -5,6 +5,13 @@ import os
 if not os.path.exists('todos.txt'):
     with open('todos.txt','w') as file:
         pass
+
+todos=[]
+def add_todo():
+    todo=st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 todos=functions.get_todos()
 
 st.title("My Todo App")
@@ -14,4 +21,6 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label = "Enter a todo: ",placeholder="Add new todo...")
+st.text_input(label = "Enter a todo: ",placeholder="Add new todo...",
+              on_change=add_todo,key='new_todo')
+
