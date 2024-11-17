13daa7ac6c26468a5caf3ee93b4c574335da2d1a
saurabh <saurabhlively@yahoo.co.in>
2024-11-17 13:22:12 +0530

    Added complete in web interface to delete what is checked in checklist

--- mytoapp-web.py
+++ mytoapp-web.py
@@ -12,15 +12,28 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
+
+
+
 todos=functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+"""Below code is for removing item from ui which is checked"""
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        """Below will delete from session state dictionary"""
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
 
 st.text_input(label = "Enter a todo: ",placeholder="Add new todo...",
               on_change=add_todo,key='new_todo')
 
+st.session_state
